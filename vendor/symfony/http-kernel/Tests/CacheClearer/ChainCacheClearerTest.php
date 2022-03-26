80a9681e4920345ffad7bdf8a457090b023a47f0
Kenneth Camacho <k.camacho@globiq.com>
2022-03-26 15:55:55 +0800

    upgrade laravel 5.8

--- vendor/symfony/http-kernel/Tests/CacheClearer/ChainCacheClearerTest.php
+++ vendor/symfony/http-kernel/Tests/CacheClearer/ChainCacheClearerTest.php
@@ -11,15 +11,16 @@
 
 namespace Symfony\Component\HttpKernel\Tests\CacheClearer;
 
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpKernel\CacheClearer\ChainCacheClearer;
 
-class ChainCacheClearerTest extends \PHPUnit_Framework_TestCase
+class ChainCacheClearerTest extends TestCase
 {
     protected static $cacheDir;
 
     public static function setUpBeforeClass()
     {
-        self::$cacheDir = tempnam(sys_get_temp_dir(), 'sf2_cache_clearer_dir');
+        self::$cacheDir = tempnam(sys_get_temp_dir(), 'sf_cache_clearer_dir');
     }
 
     public static function tearDownAfterClass()
@@ -38,20 +39,8 @@ class ChainCacheClearerTest extends \PHPUnit_Framework_TestCase
         $chainClearer->clear(self::$cacheDir);
     }
 
-    public function testInjectClearerUsingAdd()
-    {
-        $clearer = $this->getMockClearer();
-        $clearer
-            ->expects($this->once())
-            ->method('clear');
-
-        $chainClearer = new ChainCacheClearer();
-        $chainClearer->add($clearer);
-        $chainClearer->clear(self::$cacheDir);
-    }
-
     protected function getMockClearer()
     {
-        return $this->getMock('Symfony\Component\HttpKernel\CacheClearer\CacheClearerInterface');
+        return $this->getMockBuilder('Symfony\Component\HttpKernel\CacheClearer\CacheClearerInterface')->getMock();
     }
 }
